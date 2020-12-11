de291c60eb27bdeec9e5d885cb8177a59903a254
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-11 09:55:15 -0700

    Remove comments from spec

--- spec/features/find_characters_spec.rb
+++ spec/features/find_characters_spec.rb
@@ -12,24 +12,14 @@ RSpec.describe 'As a user,', type: :feature do
       expect(current_path).to eq("/search")
       expect(page).to have_css(".members_information")
 
-      within(".members_information") do
+      within first(".members_information")do
         expect(page).to have_css(".name")
         expect(page).to have_css(".photo")
         expect(page).to have_css(".allies")
         expect(page).to have_css(".affiliation")
       end
 
-      expect(page).to have_css(".total_members", count: 16)
       expect(page).to have_content("Total Results: 20")
     end
   end
 end
-
-
-# Then I should see the total number of people who live in the Fire Nation. (i.e. 20)
-
-# And I should see a list with the detailed information for the 20 members of the Fire Nation.
-# And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies and a list of enemies
-# - Any affiliation that the member has
